1d28ed99604e1da28ff8e15be86a8f5b18726e7e
Cestt <electromanuel33@hotmail.com>
2015-12-10 19:10:05 +0100

    Level Editor 0.0001

---
+++ Assets/Scripts/Level Creator/New Level/Editor/NewLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using LevelUtilities;
+
+public class NewLevel{
+	[MenuItem("Tools/New Level/Empty Level")]
+	private static void NewEmptyLevel () {
+		LevelInit.NewEmptyLevel();
+	}
+	[MenuItem("Tools/New Level/Basic Level")]
+	private static void NewBasicLevel () {
+		LevelInit.NewEmptyLevel();
+	}
+}
---
+++ Assets/Scripts/Level Creator/New Level/LevelInit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+namespace LevelUtilities{
+	public class LevelInit : MonoBehaviour {
+		
+		// Creates a new scene
+		private static void NewScene () {
+			EditorApplication.SaveCurrentSceneIfUserWantsTo ();
+			EditorApplication.NewScene ();
+		}
+		// Remove all the elements of the scene
+		public static void CleanScene () {
+			GameObject[] allObjects = Object.FindObjectsOfType<GameObject>
+				();
+			foreach (GameObject go in allObjects) {
+				GameObject.DestroyImmediate (go);
+			}
+		}
+		// Creates a new scene capable to be used as a level
+		public static void NewEmptyLevel () {
+			NewScene ();
+			CleanScene ();
+			EditorApplication.SaveScene();
+		}
+	}
+}
+
+
